40fbf434677aa54777ef38f00bc8b338613d3678
ZajacPi <piotr8zajac8@gmail.com>
2024-07-06 22:45:52 +0200

    Making main menu and trying out css

--- Goldmill.py
+++ Goldmill.py
@@ -5,6 +5,14 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtGui import QCursor
 from PyQt5 import QtGui, QtCore
 
+#create a dict of global widgets
+widgets = {
+    "logo": [],
+    "button": [], 
+    "score": [],
+    "question": []
+}
+
 def main():
     app = QApplication(sys.argv)
     grid = QGridLayout()
@@ -14,27 +22,61 @@ def main():
     window.setGeometry(750,100,500, 500)
     #you can simply use css commands
     window.setStyleSheet("background-color: #2F4F4F;")
+    def main_menu():
+        logo_image = QPixmap("rocky_logo.png")
+        logo  = QLabel()
+        logo.setPixmap(logo_image)
+        #the alignment flag makes the logo fixed always in the middle of the window (horisontally, because of the H, AlignCenter makes it fixet exactly in the middle)
+        logo.setStyleSheet("margin-top: 100px;")
+        widgets["logo"].append(logo)
 
-    logo_image = QPixmap("rocky_logo.png")
-    logo  = QLabel()
-    logo.setPixmap(logo_image)
-    #the alignment flag makes the logo fixed always in the middle of the window (horisontally, because of the H, AlignCenter makes it fixet exactly in the middle)
-    logo.setStyleSheet("margin-top: 100px;")
+        #creating button widget
+        button = QPushButton("PLAY")
+        button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+        button.setStyleSheet(
+            "*{border: 4px solid '#8B0000';" +
+            "border-radius: 15px;" +
+            "font-size: 35px;" +
+            "color: 'white';" + 
+            "padding: 25px 0;}"  + 
+            #hover event
+            "*:hover{background: '#8B0000';}"
+                            )
+        # button.setStyleSheet("margin-bottom: 100px;")
+        widgets["button"].append(button)
 
-    #cerating button widget
-    button = QPushButton("PLAY")
-    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-    button.setStyleSheet(
-        "border: 4px solid 'BC006C';" +
-        "border-radius: 15px;" +
-        "font-size: 35px;" +
-        "color: 'white';"
-                         )
-    # button.setStyleSheet("margin-bottom: 100px;")
+        #I add global wigdets, [-1] for the last version
+        grid.addWidget(widgets["logo"][-1], 0, 0, alignment=QtCore.Qt.AlignHCenter)
+        grid.addWidget(widgets["button"][-1], 1, 0)
+    def frame():
+        score = QLabel("80")
+        score.setAlignment(QtCore.Qt.AlignRight)
+        score.setStyleSheet(
+             "*{border: 4px solid '#8B0000';" +
+            "border-radius: 15px;" +
+            "font-size: 35px;" +
+            "color: 'white';" + 
+            "padding: 25px 0;}"  + 
+            #hover event
+            "*:hover{background: '#8B0000';}"
+        )
+        widgets["score"].append(score)
+        question = QLabel("Pytanie za 100000 pkt")
+        question.setAlignment(QtCore.Qt.AlignCenter)
+        #some questions will be shoreter, some longer, the text should wrap
+        question.setWordWrap(True)
+        question.setStyleSheet(
+             "*{border: 4px solid '#8B0000';" +
+            "border-radius: 15px;" +
+            "font-size: 35px;" +
+            "color: 'white';" + 
+            "padding: 25px 0;}")
+        widgets["question"].append(question)
+        grid.addWidget(widgets["score"][-1], 0, 1)
+        grid.addWidget(widgets["question"][-1], 1, 0, 1, 2 )
 
-    grid.addWidget(logo, 0, 0, alignment=QtCore.Qt.AlignHCenter)
-    grid.addWidget(button, 1, 0)
-
+    # main_menu()
+    frame()
     window.setLayout(grid)
     #statement at the end to show and exit 
     window.show()
